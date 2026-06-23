783b45a59a7a08b74dce91633115c5e04932c262
shlomi-sharbet <shlomi.sharbet@gmail.com>
2026-06-24 02:56:31 +0300

    update getHighSatStudents2222

--- src/main/java/com/handson/basic/controller/StudentsController.java
+++ src/main/java/com/handson/basic/controller/StudentsController.java
@@ -96,7 +96,7 @@ public class StudentsController {
     }
 
     @RequestMapping(value = "/highSat", method = RequestMethod.GET)
-    public ResponseEntity<?> getHighSatStudents222(@RequestParam Integer sat) {
+    public ResponseEntity<?> getHighSatStudents2222(@RequestParam Integer sat) {
         return new ResponseEntity<>(studentService.getStudentWithSatHigherThan(sat), HttpStatus.OK);
     }
 
